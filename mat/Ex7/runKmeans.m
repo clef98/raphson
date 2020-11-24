96c8cc68712cbc73a12d4c531a2fce2995c51708
Starfish <67656192+SeaUrc@users.noreply.github.com>
2020-11-23 19:39:13 -0500

    Update runKmeans.m

--- mat/Ex7/runKmeans.m
+++ mat/Ex7/runKmeans.m
@@ -14,15 +14,13 @@ idx = zeros(m, 1);
 
 for i=1:max_iters
     
-    fprintf('K-Means iteration %d/%d...\n', i, max_iters);
     if exist('OCTAVE_VERSION')
         fflush(stdout);
     end
 
     idx = findClosestCentroids(X, centroids);    
-    
-    % Given the memberships, compute new centroids
     centroids = computeCentroids(X, idx, K);
+    
 end
 
 end
