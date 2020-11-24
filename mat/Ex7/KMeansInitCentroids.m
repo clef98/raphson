442613607437716655b5b07849131d90e4656dac
Starfish <67656192+SeaUrc@users.noreply.github.com>
2020-11-23 19:39:34 -0500

    Update KMeansInitCentroids.m

--- mat/Ex7/KMeansInitCentroids.m
+++ mat/Ex7/KMeansInitCentroids.m
@@ -3,7 +3,6 @@ function centroids = kMeansInitCentroids(X, K)
 
 centroids = zeros(K, size(X, 2));
 
-% Initialize the centroids to be random examples
 randidx = randperm(size(X, 1));
 centroids = X(randidx(1:K), :);
 
