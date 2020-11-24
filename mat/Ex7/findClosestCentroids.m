b117430fde66b752a926a5610b5989a5b4f26dda
Starfish <67656192+SeaUrc@users.noreply.github.com>
2020-11-23 19:38:43 -0500

    Update findClosestCentroids.m

--- mat/Ex7/findClosestCentroids.m
+++ mat/Ex7/findClosestCentroids.m
@@ -1,18 +1,19 @@
 %%Name findClosestCentroids.m
 function idx = findClosestCentroids(X, centroids)
 
-K = size(centroids, 1);    %centroids = 3x2 
+K = size(centroids, 1);  
 idx = zeros(size(X,1), 1); 
 
 for i=1:size(X,1);
+
   temp = zeros(K,1);  
   for j=1:K;
     
     temp(j) = sqrt(sum((X(i,:) - centroids(j,:)) .^ 2));
     
   endfor
-  
   [~,idx(i)] = min(temp);
+  
 endfor
 
 end
